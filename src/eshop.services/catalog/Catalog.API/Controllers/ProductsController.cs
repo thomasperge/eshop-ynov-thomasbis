b7b9998fb40a7168a48ff0925c4214cb4893b086
ThomasFzr <125970267+ThomasFzr@users.noreply.github.com>
2025-10-09 09:36:37 +0200

    Feat/get product by category (#3)
    * feat: get product by category

--- src/eshop.services/catalog/Catalog.API/Controllers/ProductsController.cs
+++ src/eshop.services/catalog/Catalog.API/Controllers/ProductsController.cs
@@ -2,7 +2,7 @@ using Catalog.API.Features.Products.Commands.CreateProduct;
 using Catalog.API.Features.Products.Commands.DeleteProduct;
 using Catalog.API.Features.Products.Commands.UpdateProduct;
 using Catalog.API.Features.Products.Queries.GetProductById;
-using Catalog.API.Features.Products.Commands.GetProductByCategory;
+using Catalog.API.Features.Products.Queries.GetProductByCategory;
 
 using Catalog.API.Models;
 using MediatR;
@@ -44,11 +44,10 @@ public class ProductsController(ISender sender) : ControllerBase
     [ProducesResponseType(typeof(BadRequestObjectResult), StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<Product>> GetProductsByCategory(string category)
     {
-        // TODO
         if (string.IsNullOrWhiteSpace(category))
             return BadRequest("Category is required");
         
-        var result = await sender.Send(new GetProductByCategoryCommand(category));
+        var result = await sender.Send(new GetProductByCategoryQuery(category));
         return Ok(result.Products);
     }
 
